6ae598dcd5c9d0f3942d4a6c06c8b12bce1ee081
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-12-29 23:57:50 -0300

    feat: add constants

--- src/MCB.Core.Domain.Entities.Abstractions/Specifications/IDomainEntitySpecifications.cs
+++ src/MCB.Core.Domain.Entities.Abstractions/Specifications/IDomainEntitySpecifications.cs
@@ -2,6 +2,12 @@
 
 public interface IDomainEntitySpecifications
 {
+    // Constants
+    public const int DOMAIN_ENTITY_CREATED_BY_MAX_LENGTH = 150;
+    public const int DOMAIN_ENTITY_LAST_UPDATED_BY_MAX_LENGTH = 150;
+    public const int DOMAIN_ENTITY_LAST_SOURCE_PLATFORM_MAX_LENGTH = 150;
+
+    // Methods
     bool IdShouldRequired(Guid id);
     bool TenantIdShouldRequired(Guid tenantId);
 
